cf9ffeaeb324c3f9050849c2011c7de9967538bb
Aren <melaren6@gmail.com>
2024-04-05 14:28:56 +0400

    table

--- src/components/TableWithCheckboxes.jsx
+++ src/components/TableWithCheckboxes.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import { useEffect, useState, useCallback } from 'react'
 
 export default function TableWithCheckboxes() {
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+  const fetchUsers = useCallback(async () => {
+    setLoading(true)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    setUsers(users)
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers])
 
   return (
+
     <div className="flex flex-col gap-3">
       <Table
         selectionMode="multiple"
         aria-label="Example static collection table"
       >
         <TableHeader>
+          <TableColumn>ID</TableColumn>
           <TableColumn>NAME</TableColumn>
-          <TableColumn>ROLE</TableColumn>
           <TableColumn>STATUS</TableColumn>
         </TableHeader>
         <TableBody>
-          <TableRow key="1">
-            <TableCell>Tony Reichert</TableCell>
-            <TableCell>CEO</TableCell>
-            <TableCell>Active</TableCell>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.id}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.name}</TableCell>
           </TableRow>
-          <TableRow key="2">
-            <TableCell>Zoey Lang</TableCell>
-            <TableCell>Technical Lead</TableCell>
-            <TableCell>Paused</TableCell>
-          </TableRow>
-          <TableRow key="3">
-            <TableCell>Jane Fisher</TableCell>
-            <TableCell>Senior Developer</TableCell>
-            <TableCell>Active</TableCell>
-          </TableRow>
-          <TableRow key="4">
-            <TableCell>William Howard</TableCell>
-            <TableCell>Community Manager</TableCell>
-            <TableCell>Vacation</TableCell>
-          </TableRow>
+        ))}
         </TableBody>
       </Table>
     </div>
